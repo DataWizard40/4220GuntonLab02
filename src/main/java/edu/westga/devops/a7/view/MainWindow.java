e72969106f607320e6396a361e963f19765f0e4e
Richard E Gunton <rg00048@my.westga.edu>
2025-04-30 10:46:09 -0400

    Add controller skeleton code

--- src/main/java/edu/westga/devops/a7/view/MainWindow.java
+++ src/main/java/edu/westga/devops/a7/view/MainWindow.java
@@ -2,7 +2,7 @@ package edu.westga.devops.a7.view;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.ListView;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert;
@@ -16,6 +16,49 @@ import edu.westga.devops.a7.model.Item;
  * @author	DevOps
  * @version Spring 2025
  */
-public class MainWindow {       
-	
+public class MainWindow {
+
+    @FXML
+    private Button addItemButton;
+
+    @FXML
+    private ListView<?> itemListView;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private TextField quantityTextField;
+
+    @FXML
+    private Button removeItemButton;
+
+    @FXML
+    private Button updateQuantityButton;
+
+    @FXML
+    void addItem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removeItem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateQuantity(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert addItemButton != null : "fx:id=\"addItemButton\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert itemListView != null : "fx:id=\"itemListView\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert nameTextField != null : "fx:id=\"nameTextField\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert quantityTextField != null : "fx:id=\"quantityTextField\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert removeItemButton != null : "fx:id=\"removeItemButton\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert updateQuantityButton != null : "fx:id=\"updateQuantityButton\" was not injected: check your FXML file 'MainWindow.fxml'.";
+
+    }
 }
